34d51feadfff2f17109cfa7cd98e818b1c53c03e
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2022-10-15 01:07:50 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,127 @@
+const TelegramApi = require('node-telegram-bot-api')
+require('dotenv').config()
+const infop = require('./requests/infop')
+const settime = require('./requests/settime')
+//const prov = require('./requests/prov')
+const bot = new TelegramApi(process.env.BOT_TOKEN, {polling: true})
+const valoper = process.env.VALOPER
+const chatId = process.env.CHATID
+let lastprop = parseInt(process.env.LASTPROPOSAL)
+let infotime = parseInt(process.env.INFOTIME)
+bot.setMyCommands([
+  {command: '/start', description: 'welcome message'},
+  {command: '/info', description: 'Information about the validator'},
+  {command: '/aprop', description: 'List of active proposals'},
+  {command: '/allprop', description: 'List of all proposals'},
+  {command: '/df', description: 'Hard disk information'},
+  {command: '/free', description: 'RAM Information'},
+  {command: '/vsync', description: 'Information about node synchronization'},
+  {command: '/settime', description: 'Frequency of sending messages for critical node errors'},
+])
+
+async function prov(){
+  let valiki = await infop('infoval',valoper)
+  let valjson = JSON.parse(valiki)
+  let vsync = await infop('vsync')
+    let vsyncjson = JSON.parse(vsync)
+  let allprop = await infop('allprop')
+  //console.log(allprop)
+  let allproparray = allprop.split('\n')
+  
+  if(valjson.jailed){
+    //сообщение, если нода попал в тюрьму
+    bot.sendMessage(chatId, 'valoper jailed');
+  }
+  
+  if(vsyncjson.SyncInfo.catching_up){
+    // сообщение, если нода не синхронизирована
+    bot.sendMessage(chatId, 'the node is not synchronized, check the synchronization information with the /vsync command');
+  }
+  allproparray.pop()
+  let lprop = allproparray[allproparray.length-1].split(' ')
+  let lpropn = parseInt(lprop[0])
+  if(lpropn > lastprop){
+    settime(lpropn,'prop')
+    lastprop = lpropn
+    //сообщение о новом пропозале
+    bot.sendMessage(chatId, `New propozal came out ${lpropn}`);
+  }
+
+}
+
+(function loop() {
+  setTimeout(function () {
+    prov()
+    loop()
+  }, infotime*1000);
+}());
+
+const start = () => {
+  bot.on('message', async msg => {
+    const text = msg.text;
+     
+    if(text === '/start'){
+      await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ed8/09d/ed809db6-5ac1-4736-9f42-b29352cc7591/2.webp')
+      return bot.sendMessage(chatId, 'Welcome to Islamic token bot!')
+    }
+    
+    if(text === '/info'){
+      valiki = await infop('infoval',valoper)
+      if(!valiki){
+        valiki = 'Validator info not found'
+      }
+      return bot.sendMessage(chatId, `${valiki}`);
+    }
+
+    if(text === '/aprop'){
+      aprop = await infop('aprop')
+      if(!aprop){
+        aprop = 'No active proposals found'
+      }
+      return bot.sendMessage(chatId, `${aprop}`);
+    }
+
+    if(text === '/allprop'){
+      allprop = await infop('allprop')
+      if(!allprop){
+        allprop = 'Proposals not found'
+      }
+      return bot.sendMessage(chatId, `${allprop}`);
+    }
+
+    if(text === '/df'){      
+      df = await infop('df')
+      if(!df){
+        df = 'no response received from the server'
+      }
+      return bot.sendMessage(chatId, 'Disc Information:\n--------------------------------------------------\n' + df);
+    }
+
+    if(text === '/vsync'){      
+      vsync = await infop('vsync')
+      if(!vsync){
+        vsync = 'no response received from the server'
+      }
+      return bot.sendMessage(chatId, 'Synchronization Information:\n--------------------------------------------------\n' + vsync);
+    }
+
+    if(text === '/free'){      
+      free = await infop('free')
+      if(!free){
+        free = 'no response received from the server'
+      }
+      return bot.sendMessage(chatId, 'RAM Information:\n--------------------------------------------------\n' + free);
+    }
+
+    if(/^\/settime\s/.test(text)){
+      let tmp = msg.text.replace(/  +/g, ' ');
+      tmp = tmp.split(' ');
+      let res = await settime(tmp[1])
+      if(res) infotime = tmp[1]
+      return bot.sendMessage(chatId, `${res}`);
+    }
+    
+    return bot.sendMessage(chatId, `Unknown command`)
+  })
+}
+start()
